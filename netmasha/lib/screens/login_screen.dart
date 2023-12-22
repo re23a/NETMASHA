6f5d03cdb3ed36970731cb88d06fe51b42d80a47
REEMA <reema.alahmari23@gmail.com>
2023-12-22 16:51:38 +0300

    splash-onboarding-login-signup-navbar

---
+++ netmasha/lib/screens/login_screen.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+import 'package:netmasha/screens/nav_bar.dart';
+import 'package:netmasha/screens/signup_screen.dart';
+import 'package:netmasha/styles/colors.dart';
+import 'package:netmasha/widgets/buttons.dart';
+import 'package:netmasha/widgets/text_field.dart';
+
+class LoginScreen extends StatelessWidget {
+  LoginScreen({super.key});
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: bg,
+      appBar: AppBar(
+        backgroundColor: purple,
+        title: Image.asset("assets/l.png"),
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(14),
+            bottomRight: Radius.circular(14),
+          ),
+        ),
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.only(top: 200),
+          child: Column(
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    child: Text(
+                      'تسجيل الدخول',
+                      style: TextStyle(
+                        color: black,
+                        fontSize: 24,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                  ),
+                  CustomTextField(
+                    controller: emailController,
+                    lableText: 'البريد الإلكتروني',
+                    hintText: '******@gmail.com',
+                  ),
+                  CustomTextField(
+                      controller: passwordController,
+                      lableText: 'كلمة المرور',
+                      hintText: '***********',
+                      isPassword: true),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "ليس لديك حساب ؟   ",
+                        style: TextStyle(
+                          color: black,
+                          fontSize: 14,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                      InkWell(
+                        onTap: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => SignUpScreen()));
+                        },
+                        child: Text(
+                          'تسجيل جديد',
+                          style: TextStyle(
+                            color: black,
+                            fontSize: 16,
+                            fontWeight: FontWeight.w400,
+                            decoration: TextDecoration.underline,
+                          ),
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              Button2(
+                txt: 'دخول',
+                onTap: () {
+                  Navigator.pushAndRemoveUntil(
+                    context,
+                    MaterialPageRoute(builder: (context) => const NavBar()),
+                    (route) => false,
+                  );
+                },
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ netmasha/lib/styles/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+Color bg = Colors.white;
+Color purple = const Color(0xFF450F9D);
+Color green = const Color(0xff9EAB52);
+Color black = const Color(0xff3D454D);
+Color stroke = const Color(0xff9D9D9D);
