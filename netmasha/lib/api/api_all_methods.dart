a1553045923abe51296c3b7e86bf33c02f5ee010
LMogdad <Zezoodj1200@gmail.com>
2023-12-27 10:50:31 +0300

    connect signup with api

--- netmasha/lib/api/api_all_methods.dart
+++ netmasha/lib/api/api_all_methods.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class Apis {
-  final String _urlApi = "https://netmasha.onrender.com";
+  final String _urlApi = "netmasha.onrender.com";
 
   Future<http.Response> postMethod({
     required Map<String, dynamic> body,
